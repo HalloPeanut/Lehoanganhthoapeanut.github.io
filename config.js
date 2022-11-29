f1d5b31ec2157ca13575f0dd7d835a605c20d034
HalloPeanut <106302012+HalloPeanut@users.noreply.github.com>
2022-11-30 02:43:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'LÊ HOÀNG ANH THOA';
+const titleCard = 'CHO CHỊ NÈ';
+const contentCard = 'ấm áp mà cả thế giới nợ chị em sẽ bù đắp hết. Chị đừng suy nghĩ nhiều quá nha <3 <3';
+
+const giftImage = 'chi.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
